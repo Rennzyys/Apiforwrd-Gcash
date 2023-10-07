e4e178486d01d0b92fcfb5ba89afc3fb43476626
renz_joshua_romano <112369021+Rennzyys@users.noreply.github.com>
2023-10-07 19:34:15 +0800

    Add files via upload

---
+++ post.php
@@ -0,0 +1,70 @@
+<?php
+
+$url = "https://api4wrd-v1.kpa.ph/paymongo/v1/create"; // you will need an app_key, get it from -> https://api4wrd.kpa.ph/register
+
+$redirect = [
+    "success" => "your-domain/success.php",
+    "failed" => "your-domain/failed.php"
+];
+
+$billing = [
+    "email" => $_GET["email"],
+    "name" =>  $_GET["firstName"] . " " .  $_GET["lastName"],
+    "phone" =>  $_GET["mobile"],
+    "address" => [
+        "line1" =>  $_GET["address"],
+        "line2" =>  $_GET["address2"],
+        "city" =>  $_GET["city"],
+        "state" =>  $_GET["state"],
+        "postal_code" =>  $_GET["zip"],
+        "country" =>  $_GET["country"]
+    ]
+];
+
+$attributes = [
+    "livemode" => false,
+    "type" => "Gcash",
+    "amount" => 10000,
+    "currency" => "PHP",
+    "redirect" => $redirect,
+    "billing" => $billing,
+];
+
+// FYI = You can use the PAYMONGO secret key & password below;
+// "secret_key" => "sk_test_HL7BiubdGVbXHXCt2nhf8fNE"
+// "password" => "your-paymongo-password"
+// sample
+
+$source = [
+    "app_key" => "697c680cd7884a16bf2e1caed977f5c3601d4fe8", // get it from -> https://api4wrd.kpa.ph/register
+    "secret_key" => "sk_test_UNcpoq8nAgfW2R8rYMpDW5zr", // secret key from paymongo - be sure your account is fully activated
+    "password" => "Newventur@phinmaed", // your paymongo account password - be sure your account is fully activated
+    "data" => [
+        "attributes" => $attributes
+    ]
+];
+
+
+$jsonData = json_encode($source);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// disable ssl
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$result = curl_exec($ch);
+$resData = json_decode($result, true);
+
+if ($resData["status"] == 200) {
+    header("Location: " . $resData["url_redirect"]);
+} else {
+    // header("Location: index.php");
+    echo $result;
+}
+
+die();
